ea80a8c5df47517182b21e07f1b706b3e18c6be9
M. Ali Akman <makman12h@gmail.com>
2022-03-24 18:13:22 +0300

    filter functionality

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
 let datadiv = document.getElementById("data");
+
+let types={deity:"purple",person:"blue",place:"green"}
 
 for (let [index, i] of data.entries()) {
 	nameclean = i.Name_clean;
 	newname = document.createElement("a");
 	newname.id = "name-" + index;
-	newname.className = "btn col s3 blue lighten-4 black-text";
+	newname.className = `btn col s3 ${types[i.Type]} ${i.Type} ${i.Writing_clean} ${i.Completeness} lighten-2 white-text`;
 	newname.innerHTML = `${nameclean}`;
 	newname.addEventListener("click", (e) => {
 		let index = e.currentTarget.id.split("-")[1];
@@ -15,17 +21,20 @@ for (let [index, i] of data.entries()) {
 
 let search = document.getElementById("search");
 
-search.addEventListener("input", (e) => {
+search.addEventListener("input", searchTags);
+
+function searchTags (e){
 	for (i of datadiv.children) {
 		let index = i.id.split("-")[1];
 		let obj = data[index];
 		if (obj.Tags_clean.includes(search.value)) {
-			i.style.display = "";
+			i.style.display=""
 		} else {
-			i.style.display = "none";
+			i.style.display="none"
 		}
 	}
-});
+	filterNames()
+}
 
 let modal = document.getElementById("modal1");
 let modalContent = document.getElementById("modal-content");
@@ -94,3 +103,83 @@ function corresMaker(obj){
 	else{
 		return ""}
 }
+
+let type_all = document.getElementById("type_all")
+let type_dn= document.getElementById("type_dn")
+let type_pn= document.getElementById("type_pn")
+let type_gn= document.getElementById("type_gn")
+let spel_all= document.getElementById("spel_all")
+let spel_phon= document.getElementById("spel_phon")
+let spel_logo= document.getElementById("spel_logo")
+let spel_akk= document.getElementById("spel_akk")
+let comp_all= document.getElementById("comp_all")
+let comp_acep= document.getElementById("comp_acep")
+let comp_incomp= document.getElementById("comp_incomp")
+
+let tips= [type_pn,type_dn,type_gn,type_all]
+let spels = [spel_phon,spel_akk,spel_logo,spel_all]
+let comps = [comp_all,comp_acep,comp_incomp]
+
+let filterArray = ["btn"]
+
+
+function filterNames(){
+	let elements = document.getElementsByClassName(filterArray.join(" "))
+	elements= Array.from(elements)
+	for (i of datadiv.children) {
+		if (elements.indexOf(i)==-1) {
+			i.style.display="none"
+		}
+
+	}
+}
+
+
+
+function filters(index,key,e,arr){
+	arr.forEach(e=>{e.classList.remove("disabled")})
+	filterArray[index]=key
+	e.currentTarget.classList.add("disabled")
+	searchTags()
+}
+
+
+type_dn.addEventListener("click",e=>{
+	filters(1,"deity",e,tips)
+})
+
+type_pn.addEventListener("click",e=>{
+	filters(1,"person",e,tips)
+})
+
+type_gn.addEventListener("click",e=>{
+	filters(1,"place",e,tips)
+})
+
+type_all.addEventListener("click",e=>{
+	filters(1,"",e,tips)
+})
+
+spel_all.addEventListener("click",e=>{
+	filters(2,"",e,spels)
+})
+
+spel_logo.addEventListener("click",e=>{
+	filters(2,"logographic",e,spels)
+})
+
+spel_phon.addEventListener("click",e=>{
+	filters(2,"phonetic",e,spels)
+})
+spel_akk.addEventListener("click",e=>{
+	filters(2,"akkadographic",e,spels)
+})
+comp_all.addEventListener("click",e=>{
+	filters(3,"",e,comps)
+})
+comp_acep.addEventListener("click",e=>{
+	filters(3,"acephalous",e,comps)
+})
+comp_incomp.addEventListener("click",e=>{
+	filters(3,"incomplete",e,comps)
+})
